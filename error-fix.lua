9545bf045fc977e789f0f30abaa6e561b9d9328a
SquidDev <bonzoweb@hotmail.co.uk>
2018-08-19 17:56:42 +0100

    Make error-fix work on most things

--- error-fix.lua
+++ error-fix.lua
@@ -6,4 +6,11 @@ local methods = {
   "__concat",
 }
 for _, method in ipairs(methods) do mt[method] = void end
+
 debug.setmetatable(nil, mt)
+debug.setmetatable(1, mt)
+debug.setmetatable(true, mt)
+debug.setmetatable(print, mt)
+
+local st = debug.getmetatable("")
+for k, v in pairs(mt) do st[k] = st[k] or v end
